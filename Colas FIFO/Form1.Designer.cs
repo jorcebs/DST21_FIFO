806014ca8b582a1e6fe2fd679ff0af7826db197c
jorcebs <jor.cebs@gmail.com>
2018-10-20 20:21:57 -0500

    Upload

---
+++ Colas FIFO/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Colas_FIFO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnComenzar = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnComenzar
+            // 
+            this.btnComenzar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnComenzar.Location = new System.Drawing.Point(13, 14);
+            this.btnComenzar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnComenzar.Name = "btnComenzar";
+            this.btnComenzar.Size = new System.Drawing.Size(112, 35);
+            this.btnComenzar.TabIndex = 0;
+            this.btnComenzar.Text = "Comenzar";
+            this.btnComenzar.UseVisualStyleBackColor = true;
+            this.btnComenzar.Click += new System.EventHandler(this.btnComenzar_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(13, 57);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(543, 127);
+            this.txtLog.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 195);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.btnComenzar);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Colas FIFO";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnComenzar;
+        private System.Windows.Forms.TextBox txtLog;
+    }
+}
+
---
+++ Colas FIFO/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Colas_FIFO
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnComenzar_Click(object sender, EventArgs e)
+        {
+            Queue<Proceso> miFifo = new Queue<Proceso> { };
+            int ciclosVacía = 0;
+            for (int i = 0; i < 200; i++)
+            {
+                // 25% de probabilidades de agregar un nuevo proceso a la cola
+                if (random.Next(4) == 0)
+                {
+                    Proceso nuevo = new Proceso(random.Next(4, 15));
+                    miFifo.Enqueue(nuevo);
+                }
+
+                // Procesa
+                if (miFifo.Count > 0)
+                {
+                    miFifo.Peek().ciclos--;
+                    if (miFifo.Peek().ciclos == 0)
+                        miFifo.Dequeue();
+                }
+                else
+                    ciclosVacía++;
+            }
+
+            // Datos
+            txtLog.Text = "Cantidad de ciclos en los que estuvo vacía la cola: " + ciclosVacía.ToString() + Environment.NewLine;
+            txtLog.Text += "Quedaron " + miFifo.Count.ToString() + " procesos pendientes" + Environment.NewLine;
+            int totalCiclosPendientes = 0;
+            foreach (Proceso proceso in miFifo)
+                totalCiclosPendientes += proceso.ciclos;
+            txtLog.Text += "Hicieron falta " + totalCiclosPendientes.ToString() + " ciclos para terminar de procesar la cola";
+        }
+    }
+}
